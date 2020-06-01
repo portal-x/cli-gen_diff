4c8477cae9b6a66135abf9ab2d8b3fa996504917
Yurii Tkachuk <papirus.borzna@gmail.com>
2020-06-01 11:56:05 +0300

    increased readability

--- src/formatters/tree.js
+++ src/formatters/tree.js
@@ -6,26 +6,32 @@ const parseObjectToString = (data) => {
   return `${key}: ${value}`;
 };
 
-const makeFormat = (list, depth) => list.reduce((acc, item) => {
-  const spaces = ' '.repeat(depth + 2);
+const genSpaces = (depth) => ' '.repeat(depth + 2);
+
+const getValue = (value, depth) => {
   const baseSpaces = ' '.repeat(6);
-  const ifNotChanged = ' '.repeat(2);
-  const oldValue = _.isObject(item.valueBefore) ? `{\n${spaces}${baseSpaces}${parseObjectToString(item.valueBefore)}\n${spaces}  }` : item.valueBefore;
-  const changedValue = _.isObject(item.valueAfter) ? `{\n${spaces}${baseSpaces}${parseObjectToString(item.valueAfter)}\n${spaces}  }` : item.valueAfter;
+  return _.isObject(value)
+    ? `{\n${genSpaces(depth)}${baseSpaces}${parseObjectToString(value)}\n${genSpaces(depth)}  }` : value;
+};
+
+const makeFormat = (list, depth) => list.reduce((acc, item) => {
+  const fill = ' '.repeat(2);
+  const oldValue = getValue(item.valueBefore, depth);
+  const changedValue = getValue(item.valueAfter, depth);
   if (item.type === 'changed') {
-    return `${acc}\n${spaces}- ${item.key}: ${oldValue}\n${spaces}+ ${item.key}: ${changedValue}`;
+    return `${acc}\n${genSpaces(depth)}- ${item.key}: ${oldValue}\n${genSpaces(depth)}+ ${item.key}: ${changedValue}`;
   }
   if (item.type === 'unchanged') {
-    return `${acc}\n${spaces}${ifNotChanged}${item.key}: ${oldValue}`;
+    return `${acc}\n${genSpaces(depth)}${fill}${item.key}: ${oldValue}`;
   }
   if (item.type === 'added') {
-    return `${acc}\n${spaces}+ ${item.key}: ${oldValue}`;
+    return `${acc}\n${genSpaces(depth)}+ ${item.key}: ${oldValue}`;
   }
   if (item.type === 'node') {
-    return `${acc}\n${spaces}${ifNotChanged}${item.key}: {${makeFormat(item.children, depth + 4)}\n${spaces}  }`;
+    return `${acc}\n${genSpaces(depth)}${fill}${item.key}: {${makeFormat(item.children, depth + 4)}\n${genSpaces(depth)}  }`;
   }
 
-  return `${acc}\n${spaces}- ${item.key}: ${oldValue}`;
+  return `${acc}\n${genSpaces(depth)}- ${item.key}: ${oldValue}`;
 }, '');
 
 export default (ast) => `{${makeFormat(ast, 0)}\n}`;
